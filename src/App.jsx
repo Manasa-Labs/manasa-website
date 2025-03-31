3506bfe8a38f7c2d74ff552fec802f7f7d9beea9
ishan-marikar <skitzophreak69@gmail.com>
2025-03-31 20:22:16 +0530

    Add new background videos and update video selection logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import manasaLogo from "./assets/logo-dark.png";
 
 function App() {
-  const videos = ["/bg.mp4", "/bg-alt.mp4"];
+  const videos = ["/bg-1.mp4", "/bg-2.mp4", "/bg-3.mp4", "/bg-4.mp4"];
   const randomVideo = videos[Math.floor(Math.random() * videos.length)];
 
   return (
