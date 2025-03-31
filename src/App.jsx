1c8fe142ecc17d4b9da74f2a6826d1ae00528a84
ishan-marikar <skitzophreak69@gmail.com>
2025-03-31 23:16:42 +0530

    Nishii doesn't like the other videos so I reverting back to the first one

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "./App.css";
 import manasaLogo from "./assets/logo-dark.png";
 
 function App() {
-  const videos = ["/bg-1.mp4", "/bg-2.mp4", "/bg-3.mp4", "/bg-4.mp4"];
+  // const videos = ["/bg-1.mp4", "/bg-2.mp4", "/bg-3.mp4", "/bg-4.mp4"];
+  const videos = ["/bg-1.mp4"];
   const randomVideo = videos[Math.floor(Math.random() * videos.length)];
 
   return (
