44b9002f3be21549f00e765eb1b3c394e1c8496f
ishan-marikar <skitzophreak69@gmail.com>
2025-07-08 12:11:01 +0530

    Switch to /next project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
+  plugins: [react(), tailwindcss()],
+  base: '/',
   build: {
-    sourcemap: false,
-  },
-});
+    outDir: 'build'
+  }
+})
